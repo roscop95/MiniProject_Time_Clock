9df60032ccfde80c53a2f2d811ea796bd655059a
roscop95 <64421645+roscop95@users.noreply.github.com>
2020-10-27 23:04:50 -0500

    Initial commit

---
+++ src/Clock.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export default class Clock extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      time: new Date().toLocaleTimeString()
+    };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tickTockClock(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tickTockClock() {
+    this.setState({
+      time: new Date().toLocaleTimeString()
+    });
+  }
+
+  render() {
+    return (
+      <div className="Clock">
+        <p> The time is {this.state.time}</p>
+      </div>
+    );
+  }
+}
